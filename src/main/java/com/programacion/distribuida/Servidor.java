21037d2911d7be5fc057cdb11e06fe2d3d794f57
LeninIbarra <pokibarra1999@gmail.com>
2023-02-17 03:00:53 -0500

    Prueba Distribuida

---
+++ src/main/java/com/programacion/distribuida/Servidor.java
@@ -0,0 +1,9 @@
+package com.programacion.distribuida;
+
+import io.quarkus.runtime.Quarkus;
+
+public class Servidor {
+    public static void main(String[] args) {
+        Quarkus.run();
+    }
+}
